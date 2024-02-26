f3fcaa4e76bd0e0bbd205bc514ff0697d8f839ea
Jhonatan Westerhofen dos Santos <61605105+Jhonatan101136@users.noreply.github.com>
2024-02-26 19:40:16 -0300

    personDTO

--- src/main/java/org/vita/health/domain/entity/dto/PersonDTO.java
+++ src/main/java/org/vita/health/domain/entity/dto/PersonDTO.java
@@ -1,7 +1,66 @@
 package org.vita.health.domain.entity.dto;
 
+import java.time.LocalDateTime;
+
 public class PersonDTO {
 
     private String name;
-    
+
+    private String firstName;
+
+    private String document;
+
+    private LocalDateTime birthday;
+
+    private boolean isPacient;
+
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public LocalDateTime getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDateTime birthday) {
+        this.birthday = birthday;
+    }
+
+    public boolean isPacient() {
+        return isPacient;
+    }
+
+    public void setPacient(boolean isPacient) {
+        this.isPacient = isPacient;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
