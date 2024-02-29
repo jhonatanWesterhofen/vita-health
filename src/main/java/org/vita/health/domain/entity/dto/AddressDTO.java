d27d28f8682ba93de6965dee2a254d5daf25f98f
Jhonatan Westerhofen dos Santos <61605105+Jhonatan101136@users.noreply.github.com>
2024-02-28 23:05:40 -0300

    commit DTO

---
+++ src/main/java/org/vita/health/domain/entity/dto/AddressDTO.java
@@ -0,0 +1,34 @@
+package org.vita.health.domain.entity.dto;
+
+public class AddressDTO {
+
+    private String address;
+
+    private String city;
+
+    private String zipCode;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+}
